13bfec3ea29fb9478f065beeec45baa90352ce19
Ben <bendavagmo@gmail.com>
2025-12-10 16:39:25 +0000

    Very Important Change

--- src/main/java/com/bendavagmo/rubytool/RubyTool.java
+++ src/main/java/com/bendavagmo/rubytool/RubyTool.java
@@ -3,21 +3,23 @@ package com.bendavagmo.rubytool;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
-// Main mod class
 @Mod(RubyTool.MODID)
 public final class RubyTool {
-    public static final String MODID = "rubytool"; // this is your mod ID
+    public static final String MODID = "rubytool";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public RubyTool(FMLJavaModLoadingContext context) {
-        // Register common setup
-        context.getModEventBus().addListener(this::commonSetup);
+        var modBusGroup = context.getModBusGroup();
+
+        // Register the common setup event
+        FMLCommonSetupEvent.getBus(modBusGroup).addListener(this::commonSetup);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("RubyTool has loaded successfully!");
     }
 }
+
